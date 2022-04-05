24f24c56ec7b819281c615135fbff836c26472ad
williamlus <67042315+williamlus@users.noreply.github.com>
2022-04-04 20:33:19 -0400

    update test.ipynb to visualize data, change parameters in script_np.py

--- script_np.py
+++ script_np.py
@@ -361,20 +361,19 @@ class Map():
 
 def generate_data():
     # Initialization of map
-    map_width,map_height=900,750 # width and height of the map
-    types=['A','B','C','D'] # types of objects
-    populations={types[i]:v for i,v in enumerate([210,150,150,150])} # populations of different types
-    max_speeds={types[i]:v for i,v in enumerate([6,3,3,4])} # max velocities of different types
-    max_accs={types[i]:v for i,v in enumerate([0.5,0.5,0.5,0.6])} # max accelerations of different types
+    map_width,map_height=1000,1000 # width and height of the map
+    types=['A','B','C','D','E','F'] # types of objects
+    populations={types[i]:v for i,v in enumerate([100,100,100,100,100,100])} # populations of different types
+    max_speeds={types[i]:v for i,v in enumerate([3,3,3,3,3,3])} # max velocities of different types
+    max_accs={types[i]:v for i,v in enumerate([0.5,0.5,0.5,0.5,0.5,0.5])} # max accelerations of different types
     rule_list=[('A','B'),('C',('A','B')),('D',('A','B','C'))] # list of rules where the first is attracted by the second (e.g. (A,B) means A->B)
-    #rule_list=[('A','B'),('C',('A','B')),('D','E'),('F',('D','E'))] # list of rules where the first is attracted by the second (e.g. (A,B) means A->B)
-    rules={rule_list[i]:p for i,p in enumerate([60,70,65])} # may attracted only if within the dist specified in the value of the rule
-    rule_probs={rule_list[i]:p for i,p in enumerate([0.7,0.7,0.8])} # probabilities of attraction if within range
+    rules={rule_list[i]:p for i,p in enumerate([50,50,50])} # may attracted only if within the dist specified in the value of the rule
+    rule_probs={rule_list[i]:p for i,p in enumerate([0.5,0.5,0.5])} # probabilities of attraction if within range
     use_GUI=False # use GUI or not
 
     map=Map(map_width,map_height,types,populations,max_speeds,max_accs,rules,rule_probs,use_GUI)
     print(map.rules)
-    n_iters=10 # originally is 1000
+    n_iters=1000 # originally is 1000
     # suppose we want to study A ->(A,B) in this case
     # a list containing the density for chosen A for all iterations    
     densities={}
